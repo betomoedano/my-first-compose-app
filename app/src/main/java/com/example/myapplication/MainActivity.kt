fd1f424584a3b59eacfdc50b76e67c1dbf5fedfd
Beto <betomoedano@outlook.com>
2026-05-29 18:03:09 -0500

    hello world

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,13 +11,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -30,19 +34,19 @@ import coil3.compose.AsyncImage
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            MyApplicationTheme {
-                MyApp(onClick = { onClick() })
-            }
-        }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContent {
+      MyApplicationTheme {
+        MyApp(onClick = { onClick() })
+      }
     }
+  }
 
-    private fun onClick() {
-        Log.d("[Beto Logs]", "Beto was here!")
-    }
+  private fun onClick() {
+    Log.d("[Beto Logs]", "Beto was here!")
+  }
 }
 
 
@@ -50,44 +54,55 @@ class MainActivity : ComponentActivity() {
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun MyApp(onClick: () -> Unit = {}) {
-    // Define the missing state for the navigation suite
-    var currentDestination by remember { mutableStateOf(AppDestinations.HOME) }
+  // Define the state for the current destination
+  var currentDestination by remember { mutableStateOf(AppDestinations.HOME) }
+  var presses by remember { mutableIntStateOf(0) }
 
-    NavigationSuiteScaffold(
-        navigationItems = {
-            AppDestinations.entries.forEach { destination ->
-                item(
-                    icon = {
-                        Icon(
-                            destination.icon,
-                            contentDescription = destination.label
-                        )
-                    },
-                    label = { Text(destination.label) },
-                    selected = destination == currentDestination,
-                    onClick = { currentDestination = destination }
-                )
-            }
-        }
-    ) {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            BetoPic(
-                modifier = Modifier.padding(innerPadding)
+  NavigationSuiteScaffold(
+    navigationSuiteItems = {
+      AppDestinations.entries.forEach { destination ->
+        item(
+          icon = {
+            Icon(
+              destination.icon,
+              contentDescription = destination.label
             )
-        }
+          },
+          label = { Text(destination.label) },
+          selected = destination == currentDestination,
+          onClick = { currentDestination = destination }
+        )
+      }
     }
+  ) {
+    Scaffold(
+      modifier = Modifier.fillMaxSize(),
+      floatingActionButton = {
+        FloatingActionButton(onClick = {
+          presses++
+          onClick()
+        }) {
+          Icon(Icons.Default.Add, contentDescription = "Add")
+        }
+      }
+    ) { innerPadding ->
+      BetoPic(
+        modifier = Modifier.padding(innerPadding)
+      )
+    }
+  }
 }
 
 
 @Composable
 fun BetoPic(modifier: Modifier = Modifier) {
-    AsyncImage(
-        model = "https://github.com/betomoedano.png",
-        contentDescription = "Profile picture",
-        contentScale = ContentScale.Crop,
-        modifier = modifier
-            .size(120.dp)
-            .clip(CircleShape)
-            .border(2.dp, Color.White, CircleShape)
-    )
+  AsyncImage(
+    model = "https://github.com/betomoedano.png",
+    contentDescription = "Profile picture",
+    contentScale = ContentScale.Crop,
+    modifier = modifier
+      .size(120.dp)
+      .clip(CircleShape)
+      .border(2.dp, Color.White, CircleShape)
+  )
 }
